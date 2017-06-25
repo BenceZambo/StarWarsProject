a5f1576c6c4f10eb1f0d71e5226e51e4a08bd772
BenceZambo <bence.zambo96@gmail.com>
2017-06-25 22:11:01 +0200

    signup html created, in main.py os and session imported

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/sign_up')
+def sign_up():
+    pass
 
 
 if __name__ == '__main__':
