46f617e8437384b28b0225c5d1e2f05c66165c6d
BenceZambo <bence.zambo96@gmail.com>
2017-06-26 00:27:56 +0200

    sign up done

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from database_handler import *
 import os
 
 app = Flask(__name__)
@@ -12,7 +15,26 @@ def index():
 
 @app.route('/sign_up')
 def sign_up():
-    pass
+    return render_template('signup.html')
+
+
+@app.route('/sign_up_to_database', methods=["POST"])
+def sign_up_to_database():
+    get_username = request.form['username']
+    get_password = request.form['password']
+    query = """SELECT username
+            FROM SW_user
+            WHERE username = '{}'""".format(username)
+    result = handle_database(query)
+    if result == []:
+        password = generate_password_hash(user_password)
+        query = """INSERT INTO SW_user (username, password)
+                VALUES ('{0}', '{1}')""".format(user_username, password)
+        handle_database(query)
+        return render_template('login.html')
+    else:
+        return render_template('signup.html')
+
 
 
 if __name__ == '__main__':
